e6fdf1307c23d7b6aadd4ac9ba503816df8d7e01
Aayam <getaayam@gmail.com>
2022-01-29 19:22:14 +0545

    Initialized Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
-const Map = () => {
-  return <div className="map">I am a map</div>;
+import "../Map.css";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+const Map = ({ center, zoom }) => {
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </LeafletMap>
+    </div>
+  );
 };
 
 export default Map;
