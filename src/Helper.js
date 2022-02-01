a88654875ed850721b36ba1950eba1e1b58e5570
Aayam <getaayam@gmail.com>
2022-02-01 21:08:40 +0545

    added Circle in Maps

--- src/Helper.js
+++ src/Helper.js
@@ -1,3 +1,23 @@
+import { Circle } from "react-leaflet";
+
 export function numberWithCommas(cases) {
   return cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+const caseTypeColors = {
+  cases: {
+    hex: "#CC1034",
+    multiplier: 400,
+  },
+};
+
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      color={caseTypeColors[casesType].hex}
+      fillColor={caseTypeColors[casesType].hex}
+      fillOpacity={0.4}
+      radius={Math.sqrt(2) * 18}
+    ></Circle>
+  ));
