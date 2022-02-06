dc425597e7d90a211f486acbca3e5c7ed6eb8920
Aayam <getaayam@gmail.com>
2022-02-06 14:29:25 +0545

    added Icons

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,11 +1,24 @@
 import { Card, CardContent, Typography } from "@mui/material";
 import React from "react";
-const Stats = ({ title, cases, total }) => {
+import "../App.css";
+const Stats = ({ title, cases, total, icon }) => {
   return (
     <Card className="stat-box">
-      <CardContent>
-        <Typography className="stat-title" color="textSecondary">
-          <div style={{ display: "flex" }}> {title}</div>
+      <CardContent className="card">
+        <Typography
+          className="stat-title"
+          color="textSecondary"
+          style={{ fontSize: "1.2rem" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            {icon}
+            {title}
+          </div>
         </Typography>
         <h2 className="stat-case">{cases}</h2>
         <Typography className="stat-total" color="textSecondary">
